dc083becc1bea5978d78d543f7b16b062c614a5f
dongmin <dmkim9sa@gmail.com>
2021-09-11 15:14:08 +0900

    #19 Add OrderService

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    // 주문
+    @Transactional(readOnly = false)
+    public Long order(Long memberId, Long itemId, int count) {
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setDeliveryStatus(DeliveryStatus.READY);
+
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order); // CascadeType.ALL 때문에 연쇄적으로 persist()!
+        return order.getId();
+    }
+
+    // 취소
+    @Transactional(readOnly = false)
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+    // 조회
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
+}
