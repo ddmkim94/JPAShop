306006001d989c8292dd42c5ff8c8ce281ee045d
dongmin <dmkim9sa@gmail.com>
2021-09-17 15:32:45 +0900

    #30 BookForm 객체 추가

---
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+
+    private String name;
+    private int price; // 가격
+    private int stockQuantity; // 재고
+
+    private String author;
+    private String isbn;
+}
